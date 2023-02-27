5fc122452eeb73d6e9b51e1f783d82267e7cba36
Doniyorbek <ac29526@akfauniversity.org>
2023-02-27 18:20:04 +0500

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Doniyorbek Ibrokhimov  on 27/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
